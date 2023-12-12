97bd3fd959140dea0cdd28a87e105b20c5c80c1c
59335 <593353670@qq.com>
2023-12-12 15:51:07 +0800

    resnet50完成

--- main.py
+++ main.py
@@ -3,53 +3,23 @@ import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
-import cv2
-
 import models.resnet50.model as resnet50
 
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
-# 加载MNIST数据集
-train_dataset = dsets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
-test_dataset = dsets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
 
-# 加载数据集
-train_loader = DataLoader(dataset=train_dataset, batch_size=100, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=100, shuffle=False)
+def get_mnist():
+    train_dataset = dsets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+    test_dataset = dsets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
 
-modeller = resnet50.Modeller(None, device)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False)
 
-
-def train_epoch() -> None:
-    for model_input, expect in train_loader:
-        temp_input = []
-        for pic in model_input:
-            temp_pic = pic[0].numpy()
-            temp_pic = cv2.resize(temp_pic, (224, 224), interpolation=cv2.INTER_NEAREST)
-            temp_pic = temp_pic.tolist()
-
-            temp_input.append([temp_pic, temp_pic, temp_pic])
-
-        temp_expect = expect.tolist()
-        modeller.train(torch.tensor(temp_input), torch.tensor(temp_expect))
+    return train_loader, None, test_loader
 
 
-def test_epoch() -> int:
-    correct = 0
-    for model_input, expect in test_loader:
-        temp_input = []
-        for pic in model_input:
-            temp_pic = pic[0].numpy()
-            temp_pic = cv2.resize(temp_pic, (224, 224), interpolation=cv2.INTER_NEAREST)
-            temp_pic = temp_pic.tolist()
-
-            temp_input.append([temp_pic, temp_pic, temp_pic])
-
-        temp_expect = expect.tolist()
-        correct += modeller.test(torch.tensor(temp_input), torch.tensor(temp_expect))
-
-    return correct
-
-
-train_epoch()
-test_epoch()
+if __name__ == "__main__":
+    train, _, test = get_mnist()
+    modeller = resnet50.Modeller(device)
+    modeller.train(train)
+    modeller.test(test)
--- models/resnet50/model.py
+++ models/resnet50/model.py
@@ -1,65 +1,154 @@
 import torch
 import torch.nn as nn
 
+import models.resizer.model as resizer
 
-class _MyResnet50(nn.Module):
-    def __init__(self):
+
+class Residual(nn.Module):
+    def __init__(self, input_dep: int, layer1: tuple, layer2: tuple, layer3: tuple, down_sample: bool, stride: int,
+                 device):
         super().__init__()
 
-        self.linear1 = nn.Linear(in_features=3*224*224, out_features=10)
+        self.down_sample = down_sample
+        if down_sample:
+            self.conv2d0 = nn.Conv2d(input_dep, layer3[0], 1, stride=stride, padding=0, device=device)
+            self.norm2d0 = nn.BatchNorm2d(layer3[0], device=device)
+
+        self.conv2d1 = nn.Conv2d(input_dep, layer1[0], layer1[1], stride=stride if down_sample else 1,
+                                 padding=(layer1[1] - 1) // 2, device=device)
+        self.norm2d1 = nn.BatchNorm2d(layer1[0], device=device)
+        self.Relu001 = nn.ReLU()
+
+        self.conv2d2 = nn.Conv2d(layer1[0], layer2[0], layer2[1], padding=(layer2[1] - 1) // 2, device=device)
+        self.norm2d2 = nn.BatchNorm2d(layer2[0], device=device)
+        self.Relu002 = nn.ReLU()
+
+        self.conv2d3 = nn.Conv2d(layer2[0], layer3[0], layer3[1], padding=(layer3[1] - 1) // 2, device=device)
+        self.norm2d3 = nn.BatchNorm2d(layer3[0], device=device)
+        self.Relu003 = nn.ReLU()
 
     def forward(self, model_input):
-        model_input = model_input.view(-1, 3*224*224)
+        residual = model_input
+        if self.down_sample:
+            residual = self.norm2d0(self.conv2d0(model_input))
 
-        x1 = self.linear1(model_input)
-        return x1
+        layer1 = self.Relu001(self.norm2d1(self.conv2d1(model_input)))
+        layer2 = self.Relu002(self.norm2d2(self.conv2d2(layer1)))
+        layer3 = self.Relu003(self.norm2d3(self.conv2d3(layer2)))
+
+        return layer3 + residual
+
+
+class Stage(nn.Module):
+    def __init__(self, input_dep: int, layer1: tuple, layer2: tuple, layer3: tuple, residual_num: int, stride: int,
+                 device):
+        super().__init__()
+
+        self.residuals = [
+            Residual(input_dep if i == 0 else layer3[0], layer1, layer2, layer3, i == 0, stride, device=device)
+            for i in range(residual_num)
+        ]
+
+    def forward(self, model_input):
+        model_output = model_input
+        for residual in self.residuals:
+            model_output = residual(model_output)
+
+        return model_output
+
+
+class Resnet50(nn.Module):
+    def __init__(self, device):
+        super().__init__()
+
+        self.conv2d1 = nn.Conv2d(3, 64, 7, stride=2, padding=3, device=device)
+        self.maxP2d1 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.stage1 = Stage(64, (64, 1), (64, 3), (256, 1), 3, 1, device=device)
+        self.stage2 = Stage(256, (128, 1), (128, 3), (512, 1), 4, 2, device=device)
+        self.stage3 = Stage(512, (256, 1), (256, 3), (1024, 1), 6, 2, device=device)
+        self.stage4 = Stage(1024, (512, 1), (512, 3), (2048, 1), 3, 2, device=device)
+
+        self.avgP2d = nn.AvgPool2d(7, 7)
+        self.liner1 = nn.Linear(2048, 10, device=device)
+
+    def forward(self, model_input):
+        x = model_input
+
+        x = self.conv2d1(x)
+        x = self.maxP2d1(x)
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        x = self.avgP2d(x)
+        x = x.view(-1, 2048)
+        x = self.liner1(x)
+
+        return x
 
 
 class Modeller:
-    def __init__(self, input_model=None | str, device='cpu'):
-        self.model = input_model
-        if type(self.model) == str:
-            self.model = str(self.model)
-            self.model = torch.load(self.model)
+    def __init__(self, device, model_path=None | str):
+        if type(model_path) == str:
+            self.model = torch.load(str(model_path))
         else:
-            self.model = _MyResnet50()
+            self.model = Resnet50(device=device)
 
         self.model = self.model.to(device)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.1)
         self.device = device
 
+        self.resizer = resizer.Modeller()
+
     def save(self, path) -> None:
         torch.save(self.model, path)
 
-    def train(self, batch_input: torch.Tensor, batch_expect: torch.Tensor) -> None:
+    def train(self, data) -> None:
         self.model.train()
 
-        batch_input = batch_input.to(self.device)
-        batch_expect = batch_expect.to(self.device)
+        i = 1
 
-        batch_output = self.model(batch_input)
-        loss = self.criterion(batch_output, batch_expect)
+        for batch_input, batch_expect in data:
+            batch_input = self.resizer.run(batch_input.to(self.device))
+            batch_expect = batch_expect.to(self.device)
 
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+            batch_output = self.model(batch_input)
+            loss = self.criterion(batch_output, batch_expect)
 
-        print("train:")
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-    def test(self, batch_input: torch.Tensor, batch_expect: torch.Tensor) -> int:
+            print("train:step {i}".format(i=i))
+            i += 1
+
+        if self.device == 'cuda':
+            torch.cuda.empty_cache()
+
+        print("train:epoch done")
+
+    def test(self, data) -> int:
         self.model.eval()
 
-        batch_input = batch_input.to(self.device)
-        batch_expect = batch_expect.to(self.device)
-
         correct = 0
+        total = 0
 
         with torch.no_grad():
-            batch_output = self.model(batch_input)
-            _, answer = torch.max(batch_output.data, 1)
+            for batch_input, batch_expect in data:
+                batch_input = self.resizer.run(batch_input.to(self.device))
+                batch_expect = batch_expect.to(self.device)
 
-            correct += (answer == batch_expect).sum().item()
+                batch_output = self.model(batch_input)
+                _, answer = torch.max(batch_output.data, 1)
 
-        print("test:\tcorrect={correct}".format(correct=correct))
-        return correct
+                correct += (answer == batch_expect).sum().item()
+                total += 1
+
+        if self.device == 'cuda':
+            torch.cuda.empty_cache()
+
+        print("test:\tcorrect_rate {correct_rate}".format(correct_rate=correct/total))
